ae680320b3a2ccca56cd6a3da42f2d14374287a7
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-01 13:40:04 +0200

    Created useful tools for url parsing

---
+++ utils/url_tools.js
@@ -0,0 +1,13 @@
+const { URL } = require('url')
+
+function get_search_params (url) {
+    const params = new URL(url).search
+    const url_params = new URLSearchParams(params)
+    return url_params
+}
+
+function search_params_to_dict (search_params) {
+    return Object.fromEntries(search_params.entries());
+}
+
+module.exports = {get_search_params, search_params_to_dict}
