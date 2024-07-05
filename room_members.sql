6f701aedb5729d71e61180e39b4607ffb923d52a
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-05 18:51:16 +0200

    Update to database structure
    Admin user is now stored on rooms objects

--- room_members.sql
+++ room_members.sql
@@ -1,9 +1,8 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "room_members" (
-    "room" TEXT NOT NULL,
-    "user" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "voted_game" INTEGER,
-    FOREIGN KEY ("room") REFERENCES "rooms"("id")
+	"room"	TEXT NOT NULL,
+	"user"	TEXT NOT NULL,
+	"voted_game"	INTEGER,
+	FOREIGN KEY("room") REFERENCES "rooms"("id")
 );
 COMMIT;
