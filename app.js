c6c0a03a156fc99403ae2f18d91369a4185b0dda
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-07 20:54:37 +0200

    Updated app.js
    Minor change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use('/api', api);
 
 const server = http.createServer(app)
-const wss = WebSocketServer(server)
+WebSocketServer(server)
 
 // Add more route files as needed
 
