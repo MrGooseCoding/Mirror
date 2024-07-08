76725e5cf5651de63cdfde4493c798ac2e777727
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-08 20:38:20 +0200

    Database bugfix

--- room_members.sql
+++ room_members.sql
@@ -1,9 +1,10 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "room_members" (
 	"room"	TEXT NOT NULL,
-	"user"	INTEGER NOT NULL UNIQUE,
-	"redirection_key"	INTEGER NOT NULL UNIQUE,
+	"user"	TEXT NOT NULL UNIQUE,
+	"redirection_key"	TEXT NOT NULL UNIQUE,
 	FOREIGN KEY("room") REFERENCES "rooms"("id"),
+	FOREIGN KEY("user") REFERENCES "users"("id"),
 	PRIMARY KEY("user")
 );
 COMMIT;
