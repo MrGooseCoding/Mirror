43b9334bc20e89616c31c42ea62f0bc2cc8ec84e
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-13 14:13:38 +0200

    Impostor ws update
    Added all user have joined check and on close method

--- ws/routes/games/impostor.js
+++ ws/routes/games/impostor.js
@@ -12,8 +12,54 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
     const member = model_params.roommember
     const member_json = await member.json()
 
-    console.log("Hey")
-    
+    const room_id = member.getAttr("room")
+    const room = await Room.objects_getBy("id", room_id)
+
+    // The ws of the first user who joins is going to run the logic
+
+    // This starts a 5 second countdown once the first member joins
+    const status = roomStorage.getAttr("status", "start")
+    if (status === "start") {
+        roomStorage.setAttr("status", "joining_countdown")
+        setTimeout(async () => {
+            await room.refresh()
+            const member_count = await room.getMemberCount()
+            const joined_count = await ws.count_all({
+                room: room_id
+            })
+            
+            if (member_count != joined_count) {
+                await ws.send_all({
+                    room: room_id
+                }, { type: "error", error: "Missing users"})
+                await ws.close_all({
+                    room: room_id
+                })
+
+                // Because some room members haven't joined, we must delete them manually
+                await RoomMember.objects_deleteBy("room", room_id)
+                return
+            }
+            roomStorage.setAttr("status", "inside_game")
+        }, 5000)
+    }
+
+    // Once a member disconnects or is forced to disconnect, then the whole game is over
+    // However, the ws.close_all will not be executed here
+    ws.on('close', async () => {
+        await room.refresh()
+        await RoomMember.objects_deleteBy('user', member_json.id)
+
+        const joined_count = await ws.count_all({
+            room: room_id
+        })
+        if (joined_count === 0) {
+            roomStorage.empty()
+            await Room.objects_deleteBy('id', room_id)
+            return
+        } 
+    });
+
 }, {
     required_parameters: ["key"],
     auth: false,
@@ -24,8 +70,17 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
             "model": RoomMember
         }
     ],
-    inner_logic_validation: async (user, model_params, url_params) => {},
-    storage_identifier: async (user, model_params, url_params) => await model_params.roommember.json().room
+    inner_logic_validation: async (user, model_params, url_params) => {
+        // Check that the user has joined the correct game
+        const member = model_params.roommember
+        const room = await Room.objects_getBy("id", member.getAttr("room"))
+
+        if (room.getAttr("game") !== 'impostor') {
+            return "You have not joined your room's game"
+        }
+    },
+    // In this case we will use room id as the identifier
+    room_identifier: async (user, model_params, url_params) => await model_params.roommember.getAttr("room")
 })
 
 module.exports = wsRouter
