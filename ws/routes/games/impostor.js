eb1716b20dd9cb0f6f28780a2ed909d321de5506
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-16 19:46:52 +0200

    Added and sending game initial values
    - Topic
    - Role
    - Member order
    - First turn

--- ws/routes/games/impostor.js
+++ ws/routes/games/impostor.js
@@ -2,8 +2,12 @@ const Room = require('./../../../models/room')
 const RoomMember = require('./../../../models/room_member')
 const User = require('./../../../models/user')
 const WebSocketRouter = require('./../../router');
-const { count_votes } = require('./../../../utils/other');
+const { count_votes, shuffle_array } = require('./../../../utils/other');
 const { generate_random_integer } = require('./../../../utils/generators')
+const { games_config } = require('./../../../config')
+
+const impostor_config = games_config.impostor
+
 const wsRouter = new WebSocketRouter()
 
 /**
@@ -39,23 +43,37 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
                 await ws.close()
                 return
             }
-
             
+            const topics = impostor_config.topics
             const members = await room.getMembers()
-            const random_index = generate_random_integer(0, members.length)
-            const impostor = members[random_index]
+        
+            // Choosing a random topic and sending it to all users
+            const random_topic_index = generate_random_integer(0, topics.length)
+            const topic = topics[random_topic_index]
 
+            roomStorage.setAttr("topic", topic)
+            
+            await ws.send_all({
+                room: room_id
+            }, {
+                type: "topic",
+                data: topic
+            })
+
+            // Choosing roles
+
+            const random_member_index = generate_random_integer(0, members.length)
+            const impostor = members[random_member_index]
+            
             var crew = member
-            delete crew[random_index]
-
+            delete crew[random_member_index]
+            
             roomStorage.setAttr("impostor", impostor)
             roomStorage.setAttr("crew", crew)
             
-            // Sending everyone their roles
             await ws.for_all_clients({
                 room: room_id
             }, async (c) => {
-
                 if (c.getAttr("user") == impostor.getAttr("redirection_key")) {
                     await c.send({
                         type: "role",
@@ -69,20 +87,60 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
                 }
             })
             
+            // Sorting members randomly (random order)
+            
+            const member_order = shuffle_array(members)
+            
+            roomStorage.setAttr("members", member_order)
+            
+            const member_order_json = []
+            for await (let member of member_order) {
+                const member_json = await member.json()
+                member_order_json.push(member_json)
+            }
+
+            // Sending order
+            await ws.send_all({
+                room: room_id
+            }, {
+                type: "member_order",
+                data: member_order_json
+            })
+            
+            
             roomStorage.setAttr("status", "inside_game")
+            
+            await ws.send_all({
+                room: room_id,
+            },{
+                type: "inside_game"
+            })
+
+            roomStorage.setAttr("turn", 0)
+            const first_turn = member_order[0]
+            await ws.send_all({
+                room: room_id
+            }, {
+                type: "turn",
+                data: first_turn.getAttr("user")
+            })
+
         }, 5000)
     }
 
-    ws.on("message", async () => {
+    ws.on("message", async (message) => {
         status = roomStorage.getAttr("status")
         if (status != "inside_game") {
             ws.send({
                 type: "error",
-                data: "Cannot do that while not inside game"
+                data: "Cannot do that while not status is not inside_game"
             })
             return
         }
 
+        if (message.type == "word") {
+            
+        }
     })
     
     // Once a member disconnects or is forced to disconnect, then the whole game is over
