231fcfa9ba1aea77e74631d8b7950d4b57861d95
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-16 18:36:37 +0200

    Impostor game update
    - Role distribution
    - On message updated
    - On close updated

--- ws/routes/games/impostor.js
+++ ws/routes/games/impostor.js
@@ -3,6 +3,7 @@ const RoomMember = require('./../../../models/room_member')
 const User = require('./../../../models/user')
 const WebSocketRouter = require('./../../router');
 const { count_votes } = require('./../../../utils/other');
+const { generate_random_integer } = require('./../../../utils/generators')
 const wsRouter = new WebSocketRouter()
 
 /**
@@ -15,11 +16,14 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
     const room_id = member.getAttr("room")
     const room = await Room.objects_getBy("id", room_id)
 
-    // The ws of the first user who joins is going to run the logic
+    // The ws of the first user who joins is going to run the logic. We'll name it "main"
 
     // This starts a 5 second countdown once the first member joins
-    const status = roomStorage.getAttr("status", "start")
-    if (status === "start") {
+    var status = roomStorage.getAttr("status", "start")
+
+    const is_main = status === "start"
+
+    if (is_main) {
         roomStorage.setAttr("status", "joining_countdown")
         setTimeout(async () => {
             await room.refresh()
@@ -31,33 +35,82 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
             if (member_count != joined_count) {
                 await ws.send_all({
                     room: room_id
-                }, { type: "error", error: "Missing users"})
-                await ws.close_all({
-                    room: room_id
-                })
-
-                // Because some room members haven't joined, we must delete them manually
-                await RoomMember.objects_deleteBy("room", room_id)
+                }, { type: "error", data: "Missing users" })
+                await ws.close()
                 return
             }
+
+            
+            const members = await room.getMembers()
+            const random_index = generate_random_integer(0, members.length)
+            const impostor = members[random_index]
+
+            var crew = member
+            delete crew[random_index]
+
+            roomStorage.setAttr("impostor", impostor)
+            roomStorage.setAttr("crew", crew)
+            
+            // Sending everyone their roles
+            await ws.for_all_clients({
+                room: room_id
+            }, async (c) => {
+
+                if (c.getAttr("user") == impostor.getAttr("redirection_key")) {
+                    await c.send({
+                        type: "role",
+                        data: "impostor"
+                    })
+                } else {
+                    await c.send({
+                        type: "role",
+                        data: "crew"
+                    })
+                }
+            })
+            
             roomStorage.setAttr("status", "inside_game")
         }, 5000)
     }
 
+    ws.on("message", async () => {
+        status = roomStorage.getAttr("status")
+        if (status != "inside_game") {
+            ws.send({
+                type: "error",
+                data: "Cannot do that while not inside game"
+            })
+            return
+        }
+
+    })
+    
     // Once a member disconnects or is forced to disconnect, then the whole game is over
-    // However, the ws.close_all will not be executed here
     ws.on('close', async () => {
         await room.refresh()
         await RoomMember.objects_deleteBy('user', member_json.id)
-
+        
         const joined_count = await ws.count_all({
             room: room_id
         })
+
+        await ws.send_all({
+            type: "error",
+            data: "Member left during game"
+        })
+        
         if (joined_count === 0) {
             roomStorage.empty()
+            
+            // Because some room members haven't joined, we must delete them manually
+            await RoomMember.objects_deleteBy("room", room_id)
             await Room.objects_deleteBy('id', room_id)
             return
-        } 
+        }
+
+        await ws.close_all({
+            room: room_id
+        })
     });
 
 }, {
@@ -80,7 +133,8 @@ wsRouter.ws('/impostor/', async (ws, u, model_params, parameters, roomStorage) =
         }
     },
     // In this case we will use room id as the identifier
-    room_identifier: async (user, model_params, url_params) => await model_params.roommember.getAttr("room")
+    room_identifier: async (user, model_params, url_params) => await model_params.roommember.getAttr("room"),
+    user_identifier: async (user, model_params, url_params) => await url_params.key
 })
 
 module.exports = wsRouter
