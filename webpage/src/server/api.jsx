165c1e0456556a1ab4d8b328087ae0727d781dac
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-25 12:53:55 +0200

    Api grouped on api file

---
+++ webpage/src/server/api.jsx
@@ -0,0 +1,40 @@
+const fetchPOST = async (url, request_json) => {
+    const data = await fetch(url, {
+        method: 'POST',
+        //mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            request_json
+        )
+    })
+    return [data.ok, await data.json()]
+}
+
+async function user_getByToken(token) {
+    return await fetchPOST('http://localhost:3000/api/users/getByToken/', {
+        token
+    })
+}
+
+async function user_login(username, password) {
+    return await fetchPOST('http://localhost:3000/api/users/login/', {
+        username,
+        password
+    })
+}
+
+async function room_create(token) {
+    return await fetchPOST('http://localhost:3000/api/rooms/create/', {
+        token
+    })
+}
+
+async function room_canJoin(code, token) {
+    return await fetchPOST('http://localhost:3000/api/rooms/canJoin/' + code, {
+        token
+    })
+}
+
+export { user_getByToken, user_login, room_create, room_canJoin }
