a3cb678160800a6cb3bc4ec7054cbb04aba62d79
Mr. Goose <mrgoosepowerful@gmail.com>
2024-07-26 20:25:25 +0200

    Added button children

--- webpage/src/components/button.jsx
+++ webpage/src/components/button.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './../styles/style.css'
 
-function Button({ id, text, color, onClick }) {
+function Button({ id, text, color, onClick, children }) {
   return (
     <div className={`Button ${color}`}>
         <div className='exterior'>
             <div className='interior' id={id} onClick={onClick}>
-                { text }  
+                { text }
+                { children }  
             </div>
         </div>
     </div>
