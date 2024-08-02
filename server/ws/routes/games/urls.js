691810b010db7f9aab79b785436496120a70ef8e
Mr. Goose <mrgoosepowerful@gmail.com>
2024-08-02 15:20:05 +0200

    Updated game urls to include new game

--- server/ws/routes/games/urls.js
+++ server/ws/routes/games/urls.js
@@ -1,8 +1,10 @@
 const WebSocketRouter = require('./../../router')
 const impostor = require('./impostor')
+const hows_yours = require('./hows_yours')
 
 const router = new WebSocketRouter()
 
 router.use('', impostor)
+router.use('', hows_yours)
 
 module.exports = router
