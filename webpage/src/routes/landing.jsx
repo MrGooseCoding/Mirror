b84c9097288171ca9c4cda4e1a2bdf91e16f524b
Mr. Goose <mrgoosepowerful@gmail.com>
2024-08-25 19:54:45 +0200

    Updated landing page

--- webpage/src/routes/landing.jsx
+++ webpage/src/routes/landing.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
 import './../styles/style.css'
 import Modal from '../components/modal'
 import Input from '../components/input'
 import Button from '../components/button'
 import Icon from '../components/icon'
+import CrossDeviceCompatible from '../assets/videos/CrossDeviceCompatible.mp4'
+import IntuitiveFlow from '../assets/videos/IntuitiveFlow.mp4'
+
+import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { user_login } from '../server/api'
 
@@ -39,21 +42,25 @@ function Landing() {
                   <div className='title'>Cross-device compatible</div>
                   <div className='text'>Use the web version on your phone, tablet, or computer. It doesn't matter from where you access the web app, you can play toguether! </div>
                 </div>
-                <div className='image'></div>
+                <video className='media' muted loop autoPlay>
+                  <source src={CrossDeviceCompatible} type="video/mp4" />
+                </video>
               </div>
               <div className='section'>
                 <div className='info'>
                   <div className='title'>Simple and intuitive flow</div>
                   <div className='text'>Playing with your friends is easy! Simply create a room, send the code to your friends, and have them join! The app will then offer you a list of games you can play, so all the members can vote and start having fun </div>
                 </div>
-                <div className='image'></div>
+                <video className='media' muted loop autoPlay>
+                  <source src={IntuitiveFlow} type="video/mp4" />
+                </video>
               </div>
               <div className='section'>
                 <div className='info'>
                   <div className='title'>Games for all gamers</div>
                   <div className='text'>You will be able to find neverending entertainment of all types, from 1 to 8 players</div>
                 </div>
-                <div className='image'></div>
+                <div className='media'></div>
               </div>
             </div>
           </div>
