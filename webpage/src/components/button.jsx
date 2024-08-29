4ddbe55fd169e7943674cdbb5b3d75eebc79460a
Mr. Goose <mrgoosepowerful@gmail.com>
2024-08-29 18:56:42 +0200

    Add classname to btn

--- webpage/src/components/button.jsx
+++ webpage/src/components/button.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './../styles/style.css'
 
-function Button({ id, text, color, onClick, children, selected, notransform }) {
+function Button({ id, text, color, onClick, children, selected, notransform, className }) {
   return (
-    <div className={`Button ${color} ${selected ? 'selected': ''} ${notransform ? 'no-transform': ''}`}>
+    <div className={`Button ${color} ${selected ? 'selected': ''} ${notransform ? 'no-transform': ''} ${className}`}>
         <div className='exterior'>
             <div className='interior' id={id} onClick={onClick}>
                 { text }
