fc5d825279b28f46c8cce76a0ebce666252573ba
Mr. Goose <mrgoosepowerful@gmail.com>
2025-05-15 20:57:49 +0200

    Added descriptions to games

--- server/config.js
+++ server/config.js
@@ -27,19 +27,21 @@ Thanks,
         "impostor": {
             "players": {
                 "from" : 2,
-                "to" : 8
+                "to" : 10
             },
+            "description" : "Impostor is a game of improvisation and deduction. Among all players, an impostor is secretly picked, while the rest remain crew members. All crew members are given the same random word. For turns, each player says a word relative to the word given. The impostor doesn't know the given word, so he has to improvise one based on the crewmembers responses. The game ends guessing who the impostor was, winning the crew members if done correctly.",
             "topics": [
                 "hospitals",
-                "yellow stuff"
+                "yellow stuff",
             ]
             //"impostor_count"
         },
         "hows_yours": {
             "players": {
                 "from" : 2,
-                "to" : 8
+                "to" : 10
             },
+            "description" : "Among all players, a guesser is picked, while the rest remain crew members. All crew members are given the same random item. For turns, each player says a cuality of their item. The guesser doesn't know the given word, so he has to guess at the end of the game which item they're all talking about. The game ends guessing what the item was.",
             "topics": [
                 "phone",
                 "shoe"
