362c2aafcbfaaebbcf6a3499147b1592ce62978b
Mr. Goose <mrgoosepowerful@gmail.com>
2025-05-19 20:11:27 +0200

    Added about page

---
+++ webpage/src/routes/about.jsx
@@ -0,0 +1,15 @@
+import './../styles/style.css'
+import Header from '../components/header'
+
+function About() {
+  return (
+    <div className='About'>
+        <Header/>
+        <div className="main">
+					This page is being built yet
+        </div>
+    </div>
+  )
+}
+
+export default About
